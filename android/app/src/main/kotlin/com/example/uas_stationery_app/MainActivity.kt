cfa2003ed120bf723d51b081d560966bb030b93f
Muh Hilman Sholehudin <ahilman3107@gmail.com>
2023-06-13 03:04:59 +0700

    Basic Auth with Firebase

---
+++ android/app/src/main/kotlin/com/example/uas_stationery_app/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.uas_stationery_app
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/pages/login.dart
@@ -0,0 +1,104 @@
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:flutter/material.dart';
+
+class LoginPage extends StatefulWidget {
+  const LoginPage({super.key});
+
+  @override
+  State<LoginPage> createState() => _LoginPageState();
+}
+
+class _LoginPageState extends State<LoginPage> {
+  final emailController = TextEditingController();
+  final passwordController = TextEditingController();
+
+  @override
+  void dispose() {
+    emailController.dispose();
+    passwordController.dispose();
+    super.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Login Page'),
+      ),
+      body: SafeArea(
+          child: Column(
+        crossAxisAlignment: CrossAxisAlignment.center,
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          Center(
+            child: Container(
+              decoration: BoxDecoration(
+                  color: Colors.transparent,
+                  border: Border.all(color: Colors.black)),
+              width: MediaQuery.of(context).size.width * .8,
+              child: TextField(
+                controller: emailController,
+                style: const TextStyle(
+                  fontSize: 30,
+                  color: Colors.black,
+                  fontWeight: FontWeight.bold,
+                ),
+                decoration: const InputDecoration(
+                  hintText: 'Email',
+                  hintStyle: TextStyle(
+                    color: Colors.black,
+                    fontSize: 25,
+                  ),
+                  border: InputBorder.none,
+                ),
+              ),
+            ),
+          ),
+          SizedBox(height: 20),
+          Center(
+            child: Container(
+              decoration: BoxDecoration(
+                  color: Colors.transparent,
+                  border: Border.all(color: Colors.black)),
+              width: MediaQuery.of(context).size.width * .8,
+              child: TextField(
+                controller: passwordController,
+                style: const TextStyle(
+                  fontSize: 30,
+                  color: Colors.black,
+                  fontWeight: FontWeight.bold,
+                ),
+                decoration: const InputDecoration(
+                  hintText: 'Password',
+                  hintStyle: TextStyle(
+                    color: Colors.black,
+                    fontSize: 25,
+                  ),
+                  border: InputBorder.none,
+                ),
+              ),
+            ),
+          ),
+          ElevatedButton(
+            style: ElevatedButton.styleFrom(backgroundColor: Colors.cyan),
+            onPressed: () {
+              FirebaseAuth.instance.signInWithEmailAndPassword(
+                  email: emailController.text.trim(),
+                  password: passwordController.text.trim());
+            },
+            child: Text('Login'),
+          ),
+          ElevatedButton(
+            style: ElevatedButton.styleFrom(backgroundColor: Colors.red),
+            onPressed: () {
+              FirebaseAuth.instance.createUserWithEmailAndPassword(
+                  email: emailController.text.trim(),
+                  password: passwordController.text.trim());
+            },
+            child: Text('Signup'),
+          ),
+        ],
+      )),
+    );
+  }
+}
