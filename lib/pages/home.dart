8573c89c4539b0899cafe5bb4e9f00ebdfee4716
Muh Hilman Sholehudin <ahilman3107@gmail.com>
2023-06-16 02:00:21 +0700

    Update

--- lib/pages/home.dart
+++ lib/pages/home.dart
@@ -23,8 +23,8 @@ class HomePage extends StatelessWidget {
   }
 
   Widget _buildMobileLayout() {
-    return Flexible(
-      child: Padding(
+    return Scaffold(
+      body: Padding(
         padding: EdgeInsets.symmetric(
           horizontal: 10,
           vertical: 20,
@@ -129,100 +129,98 @@ class HomePage extends StatelessWidget {
   Widget _buildDesktopLayout() {
     return Scaffold(
       body: SafeArea(
-        child: Flexible(
-          child: Padding(
-            padding: EdgeInsets.symmetric(
-              horizontal: 20,
-              vertical: 30,
-            ),
-            child: Column(
-              crossAxisAlignment: CrossAxisAlignment.start,
-              children: [
-                SizedBox(height: 30),
-                Row(
-                  mainAxisAlignment: MainAxisAlignment.center,
-                  children: [
-                    Image.network(
-                      'https://cdn-icons-png.flaticon.com/512/3237/3237837.png',
-                      width: 150,
-                      height: 150,
-                    ),
-                  ],
-                ),
-                SizedBox(height: 15),
-                Row(
-                  mainAxisAlignment: MainAxisAlignment.center,
-                  children: [
-                    Text(
-                      'Welcome to Stationery Shop',
-                      style: TextStyle(
-                        fontSize: 32,
-                        fontWeight: FontWeight.bold,
-                      ),
-                    ),
-                  ],
-                ),
-                SizedBox(height: 25),
-                RichText(
-                  textAlign: TextAlign.justify,
-                  text: TextSpan(
-                    text:
-                        'This stationery shop is a wholesale store that provides office stationery at affordable prices. The stationery shop was founded in 2023 where people have returned to their activities in a stable manner. This stationery shop does not only cover online purchases, but accepts offline purchases.',
-                    style: TextStyle(fontSize: 16),
+        child: Padding(
+          padding: EdgeInsets.symmetric(
+            horizontal: 20,
+            vertical: 30,
+          ),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              SizedBox(height: 30),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  Image.network(
+                    'https://cdn-icons-png.flaticon.com/512/3237/3237837.png',
+                    width: 150,
+                    height: 150,
                   ),
+                ],
+              ),
+              SizedBox(height: 15),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  Text(
+                    'Welcome to Stationery Shop',
+                    style: TextStyle(
+                      fontSize: 32,
+                      fontWeight: FontWeight.bold,
+                    ),
+                  ),
+                ],
+              ),
+              SizedBox(height: 25),
+              RichText(
+                textAlign: TextAlign.justify,
+                text: TextSpan(
+                  text:
+                      'This stationery shop is a wholesale store that provides office stationery at affordable prices. The stationery shop was founded in 2023 where people have returned to their activities in a stable manner. This stationery shop does not only cover online purchases, but accepts offline purchases.',
+                  style: TextStyle(fontSize: 16),
                 ),
-                SizedBox(height: 25),
-                Row(
-                  mainAxisAlignment: MainAxisAlignment.center,
-                  children: [
-                    Text(
-                      'Contact',
-                      style: TextStyle(
-                        fontSize: 16,
-                        fontWeight: FontWeight.bold,
-                      ),
-                      textAlign: TextAlign.center,
+              ),
+              SizedBox(height: 25),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  Text(
+                    'Contact',
+                    style: TextStyle(
+                      fontSize: 16,
+                      fontWeight: FontWeight.bold,
                     ),
-                  ],
-                ),
-                SizedBox(height: 25),
-                Row(
-                  mainAxisAlignment: MainAxisAlignment.center,
-                  children: [
-                    ElevatedButton.icon(
-                      onPressed: () {},
-                      icon: Icon(Icons.facebook),
-                      label: Text('Facebook'),
-                    ),
-                    SizedBox(width: 10),
-                    ElevatedButton.icon(
-                      onPressed: () {},
-                      icon: Icon(Icons.email),
-                      label: Text('Email'),
-                    ),
-                    SizedBox(width: 10),
-                    ElevatedButton.icon(
-                      onPressed: () {},
-                      icon: Icon(Icons.call),
-                      label: Text('Whatsapp'),
-                    ),
-                    SizedBox(width: 10),
-                    Builder(builder: (BuildContext context) {
-                      return ElevatedButton.icon(
-                        onPressed: () {
-                          Navigator.of(context)
-                              .push(MaterialPageRoute(builder: ((context) {
-                            return const MapsWidget();
-                          })));
-                        },
-                        icon: Icon(Icons.location_on),
-                        label: Text('Maps'),
-                      );
-                    }),
-                  ],
-                ),
-              ],
-            ),
+                    textAlign: TextAlign.center,
+                  ),
+                ],
+              ),
+              SizedBox(height: 25),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  ElevatedButton.icon(
+                    onPressed: () {},
+                    icon: Icon(Icons.facebook),
+                    label: Text('Facebook'),
+                  ),
+                  SizedBox(width: 10),
+                  ElevatedButton.icon(
+                    onPressed: () {},
+                    icon: Icon(Icons.email),
+                    label: Text('Email'),
+                  ),
+                  SizedBox(width: 10),
+                  ElevatedButton.icon(
+                    onPressed: () {},
+                    icon: Icon(Icons.call),
+                    label: Text('Whatsapp'),
+                  ),
+                  SizedBox(width: 10),
+                  Builder(builder: (BuildContext context) {
+                    return ElevatedButton.icon(
+                      onPressed: () {
+                        Navigator.of(context)
+                            .push(MaterialPageRoute(builder: ((context) {
+                          return const MapsWidget();
+                        })));
+                      },
+                      icon: Icon(Icons.location_on),
+                      label: Text('Maps'),
+                    );
+                  }),
+                ],
+              ),
+            ],
           ),
         ),
       ),
